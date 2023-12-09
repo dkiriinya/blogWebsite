00364cf80920afafc4672de7ddd37b657365e581
dkiriinya <dkiriinya06sc@gmail.com>
2023-12-09 22:17:19 +0300

    added javascript to my file to create smooth scrolling

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,12 @@
-function redirectTo(link) {
-    window.location.href = link;
-}
+document.addEventListener("DOMContentLoaded", function() {
+    
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
